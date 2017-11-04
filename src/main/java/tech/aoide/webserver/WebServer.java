a6820b3d1a700559b63b637f72de6c5b9b4d90e5
Tianyi Ma <tn.ma@outlook.com>
2017-11-04 01:04:44 -0500

    Dev CORS

--- src/main/java/tech/aoide/webserver/WebServer.java
+++ src/main/java/tech/aoide/webserver/WebServer.java
@@ -14,4 +14,28 @@ public class WebServer {
         Spark.post("/process", ProcessAPI.processString);
     }
 
+    public static void enableCORS(final String origin, final String methods, final String headers) {
+        Spark.options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        Spark.before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+            response.type("application/json");
+        });
+    }
+
 }
