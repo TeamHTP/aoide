546fab80c52de2a02eb8181f66f7317e5afbd125
Tianyi Ma <tn.ma@outlook.com>
2017-11-05 05:28:23 -0600

    Sawtooth

--- src/main/java/tech/aoide/vm/jsinterpreter/JSVM.java
+++ src/main/java/tech/aoide/vm/jsinterpreter/JSVM.java
@@ -69,6 +69,9 @@ public class JSVM {
                     if (code.contains("(") || code.contains(")")) {
                         wave = Wave.SINE;
                     }
+                    else if (code.contains(">") || code.contains("<")) {
+                        wave = Wave.SAWTOOTH;
+                    }
                     else if (code.contains("[") || code.contains("]")) {
                         wave = Wave.SQUARE;
                     }
